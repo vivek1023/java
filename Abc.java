64e17e74fe5df8585db39696a446d1f455d2d988
vivek1023 <141036927+vivek1023@users.noreply.github.com>
2024-02-08 12:53:16 +0530

    Update Abc.java
    J

--- Abc.java
+++ Abc.java
@@ -24,3 +24,30 @@ public class Sample {
     }
 }
 ```
+
+
+
+class Car {
+    String brand;
+
+    // Constructor initializing the brand field with "Ford"
+    public Car() {
+        this.brand = "Ford";
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    void run() {
+        System.out.println("Car is running...");
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        Car ford = new Car();
+        String brandValue = ford.getBrand();
+        System.out.println("Brand: " + brandValue);
+    }
+}
